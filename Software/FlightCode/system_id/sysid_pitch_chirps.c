8fa7a0ffaae77c43140c643e5f33177700f86669
juliantheis <julian.theis@hotmail.com>
2015-08-13 09:34:30 -0500

    Chirp excitation signals for upcoming sys ID flight
    1. run @23m/s 15 seconds sweep 0.5-8.5 flap3 followed by 15 seconds sweep 0.5-8.5 flap4
    2. run @23m/s 17 seconds sweep 0.5-8.5 flap3
    3. run @27m/s 17 seconds sweep 0.5-8.5 flap4
    
    Amplitudes for larger speed are scaled by dynamic pressure and time to build up airspeed has been incorporated

--- Software/FlightCode/system_id/sysid_pitch_chirps.c
+++ Software/FlightCode/system_id/sysid_pitch_chirps.c
@@ -15,6 +15,7 @@ extern void get_system_id( double time, struct sensordata *sensorData_ptr, struc
 	static double local_time;
 	
 	static double f;
+	static double pi=3.14159265359; 
 	
 	
 if(entry_time_latched == FALSE)
@@ -23,67 +24,66 @@ if(entry_time_latched == FALSE)
 	entry_time_latched = TRUE;
 }
 local_time = time - entry_time;
-	
-		
-	//  apply to L3/R3 L4/R4    AT 27 M/S BASED ON DYNAMIC PRESSURE SCALING OF 23 M/S EXCITATION
-	if(sysid_select == 1){	
-		if (local_time < 18) 
+
+	//  apply to L4/R4    AT 27 M/S BASED ON DYNAMIC PRESSURE SCALING OF 23 M/S EXCITATION
+	if(sysid_select == 2){	
+		if ( (local_time>7.0)&&( (local_time-7.0)<17) ) 
 		{
-		f = 0.5 + (8.5-0.5)/(2.0*18.0) * (local_time);
-			if (local_time < 11 ){
-				controlData_ptr->pitch_cmd_excite = 3.6 * D2R * sin(2.0*3.14159265359 * f * (local_time));
+		f = 0.5 + (8.5-0.5)/(2.0*15.0) * (local_time-7.0);
+			if ( (local_time-7.0) < 9.03 ){
+				controlData_ptr->pitch_cmd_excite = 3.6 * D2R * cos(2.0*pi * f * (local_time-7.0));
 			}
 			else{
-				controlData_ptr->pitch_cmd_excite = 0.75 * D2R * sin(2.0*3.14159265359 * f * (local_time));
+				controlData_ptr->pitch_cmd_excite = 1.0 * D2R * cos(2.0*pi * f * (local_time-7.0));
+			}
+		controlData_ptr->l4    += controlData_ptr->pitch_cmd_excite;
+		controlData_ptr->r4    += controlData_ptr->pitch_cmd_excite;	
+		}
+	}
+		
+	//  apply to L3/R3    AT 27 M/S BASED ON DYNAMIC PRESSURE SCALING OF 23 M/S EXCITATION
+	if(sysid_select == 1){	
+		if ( (local_time>7.0)&&( (local_time-7.0)<17) )
+		{
+		f = 0.5 + (8.5-0.5)/(2.0*15.0) * (local_time-7.0);
+			if ( (local_time-7.0) < 9.03 ){
+				controlData_ptr->pitch_cmd_excite = 3.6 * D2R * sin(2.0*pi * f * (local_time-7.0));
+			}
+			else{
+				controlData_ptr->pitch_cmd_excite = 1.0 * D2R * sin(2.0*pi * f * (local_time-7.0));
 			}
 		controlData_ptr->l3    += controlData_ptr->pitch_cmd_excite;
 		controlData_ptr->r3    += controlData_ptr->pitch_cmd_excite;	
 		}
-		else if ( (local_time > 20)&& (local_time < 38) ) 
-		{
-		f = 0.5 + (8.5-0.5)/(2.0*18.0) * (local_time-20.0);
-		if ((local_time-20.0) < 11 ){
-			controlData_ptr->pitch_cmd_excite = 3.6 * D2R * sin(2.0*3.14159265359 * f * (local_time-20.0));	
-			}
-		else{
-			controlData_ptr->pitch_cmd_excite = 0.75 * D2R * sin(2.0*3.14159265359 * f * (local_time-20.0));
-		}			
-		controlData_ptr->l4     +=  controlData_ptr->pitch_cmd_excite;
-		controlData_ptr->r4   	+=  controlData_ptr->pitch_cmd_excite;
-		}
-			
-		
 	}
+	
 	//  apply to L3/R3 L4/R4 AT 23 M/S BASED ON ENERGY CONTENT OF 3-2-1-1 SIGNAL IN PREVIOUS FLIGHT TESTS
 	else if(sysid_select == 0){
-		if (local_time < 18 )
+		if ( (local_time>1.0)&&(local_time-1.0)<15)
 		{
-		f = 0.5 + (8.5-0.5)/(2.0*18.0) * (local_time);
-			if (local_time < 11 ){
-				controlData_ptr->pitch_cmd_excite = 5.0 * D2R * sin(2.0*3.14159265359 * f * (local_time));
+		f = 0.5 + (8.5-0.5)/(2.0*13.0) * (local_time-1.0);
+			if ( (local_time-1.0) < 11 ){
+				controlData_ptr->pitch_cmd_excite = 5.0 * D2R * sin(2.0*pi * f * (local_time-1.0));
 			}
 			else{
-				controlData_ptr->pitch_cmd_excite = 1.0 * D2R * sin(2.0*3.14159265359 * f * (local_time));
+				controlData_ptr->pitch_cmd_excite = 1.5 * D2R * sin(2.0*pi * f * (local_time-1.0));
 			}
 			
 		controlData_ptr->l3     +=  controlData_ptr->pitch_cmd_excite;
 		controlData_ptr->r3   	+=  controlData_ptr->pitch_cmd_excite;	
 		}
-		else if ( (local_time > 20)&& (local_time < 38) )
+		else if ( (local_time > 18)&& (local_time < 33) )
 		{
-		f = 0.5 + (8.5-0.5)/(2.0*18.0) * (local_time-20.0);
-			if ((local_time-20.0) < 12 ){		
-				controlData_ptr->pitch_cmd_excite = 5.0 * D2R * sin(2.0*3.14159265359 * f * (local_time-20.0));
+		f = 0.5 + (8.5-0.5)/(2.0*13.0) * (local_time-18.0);
+			if ((local_time-18.0) < 11 ){		
+				controlData_ptr->pitch_cmd_excite = 5.0 * D2R * sin(2.0*pi * f * (local_time-18.0));
 			}
 			else{
-				controlData_ptr->pitch_cmd_excite = 1.0 * D2R * sin(2.0*3.14159265359 * f * (local_time-20.0));	
+				controlData_ptr->pitch_cmd_excite = 1.5 * D2R * sin(2.0*pi * f * (local_time-18.0));	
 			}
 		controlData_ptr->l4     += 	controlData_ptr->pitch_cmd_excite;
 		controlData_ptr->r4   	+= 	controlData_ptr->pitch_cmd_excite;
 		}			
-	}
-	// 	Pitch Command Doublets
-	else if(sysid_select == 2){
 	}
 }
 
