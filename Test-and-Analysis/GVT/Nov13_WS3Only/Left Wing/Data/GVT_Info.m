7898daf5343798b75dc442a0cd14a98f78fdf944
Abhineet_Office <gupta165@umn.edu>
2015-11-13 15:18:55 -0600

    Added GVT Data for WS3;wings only

---
+++ Test-and-Analysis/GVT/Nov13_WS3Only/Left Wing/Data/GVT_Info.m
@@ -0,0 +1,120 @@
+%% Outputs GVT parameters
+% This script contains the parameters of the experiment and saves them
+% as a structure Info
+% 
+% Info has following fields
+%   - Fs:= Sampling frequency of the experiment
+%   - Run2Accel:= run-by-2 matrix with Run2Accel(i,j) providing
+%         the accelerometer number for measurement j of run i.
+%   - Accel2Run: Naccel-by-2 matrix with Accel2Run(i,1:2) providing
+%         the [Run #, Measurement #] for accelometer i.
+%   - AccelPos:= Naccel-by-2 matrix where  AccelPos(i,1) and
+%         AccelPos(i,2) are the (X,Y) positions of accel i measured
+%         in inches.  The coordinates are located at the aircraft nose
+%         with Y pointing aft and X pointing left (Hence Z is up).
+%   - ShakerPos:= 1-by-2 matrix where ShakerPos(1) and ShakerPos(2) are
+%       the (X,Y) positions of the shaker attachment point.  The 
+%       coordinates are located at the aircraft nose with Y pointing aft
+%       and X pointing left (Hence Z is up).
+%   - Nrun := Number of runs in the experiment
+%   - Nmeas := Number of measurements in each run
+%   - Naccel := Number of total acceelerations measurements
+%   - Force_V2lb := Conversion factor from measured voltage to force (Lbs)
+%   - Accel_V2g := Conversion factor from measured voltage to acceleration
+%       (g)
+%   - FreqWin+Hz := Target frequency window of the experiment
+%   - ForwardAccel := Order of the accelerometers on the leading edge from
+%      left wing tip to the right wing tip
+%   - AftAccel := Order of the accelerometers on the trailing edge from the
+%       left wing tip to the right wing tip
+
+%% Sample Info
+Fs = 2000;                      % Sampling freq, Hz
+
+%% Calibrations
+% Force: 0.4684 V/lb , Accels: 1.028 V/g
+Force_V2lb = 0.4684;
+Accel_V2g = 1.028;
+
+%% Accel Mapping
+% The data consists of Nrun experiments with each experiment providing
+% Nmeas accelerometer measurements.  The total number of accelerometer
+% measruements is thus Naccel=Nrun*Nmeas.   The accels are numbered
+% from 1 to Naccel and the position of each accelerometer is provided
+% in the array AccelPos below.  The mapping between accel numbering
+% and experiment is provided by two variables:
+%   A) Run2Accel := Nrun-by-2 matrix with Run2Accel(i,j) providing
+%         the accelerometer number for measurement j of run i.
+%   B) Accel2Run := Naccel-by-2 matrix with Accel2Run(i,1:2) providing
+%         the [Run #, Measurement #] for accelometer i.
+Run2Accel = [1 2; 3 4; 5 6; 7 8; 9 10];
+
+[Nrun,Nmeas] = size(Run2Accel);
+Naccel= Nrun*Nmeas;
+Accel2Run = zeros(Naccel,2);
+
+for i=1:Naccel;
+    [ridx,cidx]=find(Run2Accel==i);
+    Accel2Run(i,:) = [ridx cidx];
+end
+
+
+%% Accel Positions
+% As noted above, the accelerometer measurements recorded in all
+% experimental runs are numbered from 1 to Naccel.  The positions
+% of these accelerometers is provided by:
+%     AccelPos := Naccel-by-2 matrix where  AccelPos(i ,1) and
+%         AccelPos(i,2) are the (X,Y) positions of accel i measured
+%         in inches.  The coordinates are located at the aircraft nose
+%         with Y pointing aft and X pointing left (Hence Z is up).
+
+
+AccelPos = [10.51 6.30; 8.64 10.93; 19.78 10.04; 17.91 14.68; 
+            29.06 13.79; 27.18 18.43; 38.33 17.54; 36.45 22.17;
+            47.60 21.28; 45.73 25.92];
+        
+
+%% Shaker Position
+% The shaker position remains same for all the runs for this particular
+% experiment.
+%       ShakerPos := 1-by-2 matrix where ShakerPos(1) and ShakerPos(2) are
+%       the (X,Y) positions of the shaker attachment point.  The 
+%       coordinates are located at the aircraft nose with Y pointing aft
+%       and X pointing left (Hence Z is up).
+ShakerPos = AccelPos(3,1);
+
+%% Frequency Range
+% The target frequency range for which the experiment was conducted. This
+% is usually different from the actual range of the input frequencies
+% because the aircraft needs to settle down to a steady state before a
+% proper reading can be taken. For example for a traget range of  [3, 35]
+% Hz, the actual input range might be [1, 35] Hz.
+
+
+FreqWin_Hz = [3 35];
+
+%% Accel order
+% Defines the order of the accelerometers on the aircarft
+%       ForwardAccel := Accelerometer positions on the leading edge from
+%          the left wing tip to the right wing tip
+%       AftAccel := Accelerometer positions on the trailing edge from the
+%          left wing tip to the right wing tip
+
+ForwardAccel = [1:2:9];
+AftAccel = [2:2:10];
+
+%% Store Experiment Info
+
+Info.Fs = Fs;
+Info.Run2Accel = Run2Accel;
+Info.Accel2Run = Accel2Run;
+Info.AccelPos = AccelPos;
+Info.ShakerPos = ShakerPos;
+Info.Nrun = Nrun;
+Info.Nmeas = Nmeas;
+Info.Naccel = Naccel;
+Info.Force_V2lb = Force_V2lb;
+Info.Accel_V2g = Accel_V2g;
+Info.FreqWin_Hz = FreqWin_Hz;
+Info.ForwardAccel = ForwardAccel;
+Info.AftAccel = AftAccel;
---
+++ Test-and-Analysis/GVT/Nov13_WS3Only/Right Wing/Analysis/VerifyModeShape.m
@@ -0,0 +1,97 @@
+function VerifyModeShape(Info,FreqDomain,ModeFreqHz,ModeShape,fh)
+% function VerifyModeShape(FreqDomain,ModeFreqHz,ModeShape,fh)
+%
+% This function generates frequency response plots of the raw data
+% and then overlays the magnitude/phase of the estimated mode shapes.
+%
+% Inputs:
+%   Info, FreqDomain - Data structures returned by GVT_Load.
+%   ModeFreqHz - Nm-by-1 vector of estimated modal frequencies in Hz.
+%   ModeShape - Naccel-by-Nm vector of mode shapes. The i^th column,
+%             ModeShape(:,i), contains the i^th mode shape.
+%   fh - Optional input to specify the plot figure handle [Default: fh=1].
+%
+% Outputs:
+%   None
+
+% Input parsing
+nin=nargin;
+if nin==4
+    fh = 1;
+end
+
+% Grab experimental frequency response data
+Accel2Run = Info.Accel2Run;
+G = FreqDomain.FreqResponse;
+fHz = FreqDomain.FrequencyHz;
+Naccel = size(G(:,:),2);
+Nmode = numel(ModeFreqHz);
+
+% Zoom to frequency of interest
+FreqWinHz = [ModeFreqHz(1)-1 ModeFreqHz(end)+1];
+
+% Smoothed freq response in window of interest
+% This is used to generate a smoother phase plot.  The magnitude
+% plot is not smoothed as the peaks are critical in the mode id.
+Gs = G;
+Nw = 5;
+fidx = find( fHz>FreqWinHz(1) & fHz<FreqWinHz(end));
+Gs(fidx,:) = mysmooth(G(fidx,:),Nw);
+
+% Plot frequency response results for each accel
+for j=1:Naccel
+    % Estimated mode shape gain and phase
+    ModeShapeGain = 20*log10(abs(ModeShape(j,:)));
+    ModeShapePhase = 90*ones(Nmode,1);
+    ModeShapePhase( ModeShape(j,:) <0 ) = -90;
+    
+    % Get mag/phase corresponding to accel j
+    run = Accel2Run(j,1);
+    meas = Accel2Run(j,2);
+    Gmag = 20*log10(abs(G(:,run,meas)));
+    Gph = angle(G(:,run,meas))*180/pi;
+    
+    % Smoothed phase and avoid wrapping at 180degs
+%     Gphs = angle(Gs(:,run,meas));
+%     for i=1:Nmode
+%         fidxi = find( fHz>ModeFreqHz(i)-0.25 & fHz<ModeFreqHz(i)+0.25);
+%         Gphs(fidxi) = unwrap(Gphs(fidxi));
+%     end
+%     Gphs = Gphs*180/pi;        
+    Gphs = angle(Gs(:,run,meas))*180/pi; 
+    wrapangle = 25;            % Plot from [-180 180]-wrapangle
+    idx = find(Gphs>180-wrapangle); 
+    Gphs(idx)=Gphs(idx)-360;
+    
+    % Show Results
+    figure(fh);
+    
+    subplot(2,1,1)
+    semilogx(fHz,Gmag,'b',ModeFreqHz,ModeShapeGain,'ro');
+    ylabel('|G(jw)| in dB')
+    title([' Accel # = ' int2str(j)]);
+    xlim(FreqWinHz);
+    
+    idx = find(fHz>FreqWinHz(1) & fHz<FreqWinHz(2));
+    ymax = max( max(ModeShapeGain), max(Gmag(idx)) );
+    ymin = min( min(ModeShapeGain), min(Gmag(idx)) );
+    ylim([ymin ymax]);
+    
+    subplot(212)
+    semilogx(fHz,Gph,'b:',fHz,Gphs,'g',ModeFreqHz,ModeShapePhase,'ro');
+    xlabel('Frequency (Hz)')
+    ylabel('\angle G(jw) in degs')
+    xlim(FreqWinHz);
+    %ylim([-180 180]-wrapangle);
+    
+    % Print Results
+    fprintf('\nAccel # = %d \n',j);
+    for i=1:Nmode
+       fprintf('Mode = %d, Freq = %4.3f Hz, Amp=%4.3f, \n',...
+           i,ModeFreqHz(i),ModeShape(j,i))
+    end
+    
+    
+    pause
+end
+
