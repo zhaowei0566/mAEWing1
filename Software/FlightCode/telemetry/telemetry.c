d57543f1259e400a3b92f68cfc848a846051ce22
Chris Regan <rega0051@umn.edu>
2016-07-25 13:19:36 -0500

    test

--- Software/FlightCode/telemetry/telemetry.c
+++ Software/FlightCode/telemetry/telemetry.c
@@ -32,10 +32,11 @@
 #include "telemetry_interface.h"
 #include AIRCRAFT_UP1DIR
 
-#define TELE_PACKET_SIZE  51
-extern char statusMsg[103];	
+#define TELE_PACKET_SIZE 18 // 16-bit telemetry packet bytes
+#define SEND_PACKET_SIZE (2*TELE_PACKET_SIZE + 5) // 8-bit telemetry packet = [<UUT>, tele_data, <16bit_CKSUM>]
+#define STATUS_PACKET_SIZE 103
 
-/* send_telemetry packet structure = [ <UUT> time, <empty>, <empty>, p, q, r, alt, IAS, psi, theta, phi, rud, ele, thr, ail, cpuload, lon, lat, statusFlags, <empty> <16bit_CKSUM> ] */
+extern char statusMsg[STATUS_PACKET_SIZE];	
 
 static int port;
 
@@ -49,57 +50,51 @@ void send_telemetry(struct sensordata *sensorData_ptr, struct nav *navData_ptr, 
 	int bytes=0;
 	unsigned short flags=0;
 	unsigned long tmp;
-	uint16_t tele_data[36], output_CKSUM=0;
-	static byte sendpacket[TELE_PACKET_SIZE]={'U','U','T',};
+	uint16_t tele_data[TELE_PACKET_SIZE], output_CKSUM=0;
+	static byte sendpacket[SEND_PACKET_SIZE]={'U','U','T',};
 
 	// Build send_telemetry data packet
 	tmp = (unsigned long)( sensorData_ptr->imuData_ptr->time*1e04 );		// time buffer will now overflow after 59.6 hrs (thats what 4 bytes' worth!)
 	memcpy(&tele_data[0],&tmp,4);
 
-	tele_data[2] = (uint16_t)(0);
-	tele_data[3] = (uint16_t)(0);
-
-	tele_data[4] = (uint16_t)( sensorData_ptr->imuData_ptr->p*R2D / 200.0 * 0x7FFF );	//rate = 200 deg/s max saturation
-	tele_data[5] = (uint16_t)( sensorData_ptr->imuData_ptr->q*R2D / 200.0 * 0x7FFF );
-	tele_data[6] = (uint16_t)( sensorData_ptr->imuData_ptr->r*R2D / 200.0 * 0x7FFF );
+	//tele_data[2] = (uint16_t)( sensorData_ptr->imuData_ptr->p*R2D / 200.0 * 0x7FFF );	//rate = 200 deg/s max saturation
+	tele_data[2] = (uint16_t)( sensorData_ptr->imuData_ptr->q*R2D / 200.0 * 0x7FFF );
+	//tele_data[4] = (uint16_t)( sensorData_ptr->imuData_ptr->r*R2D / 200.0 * 0x7FFF );
 	
-	tele_data[7] = (uint16_t)( sensorData_ptr->imuData_ptr->ax / 100.0 * 0x7FFF );	// IMU accels = 100 m/sec2
-	tele_data[8] = (uint16_t)( sensorData_ptr->imuData_ptr->ay / 100.0 * 0x7FFF );
-	tele_data[9] = (uint16_t)( sensorData_ptr->imuData_ptr->az / 100.0 * 0x7FFF );
+	//tele_data[5] = (uint16_t)( sensorData_ptr->imuData_ptr->ax / 100.0 * 0x7FFF );	// IMU accels = 100 m/sec2
+	//tele_data[6] = (uint16_t)( sensorData_ptr->imuData_ptr->ay / 100.0 * 0x7FFF );
+	tele_data[3] = (uint16_t)( sensorData_ptr->imuData_ptr->az / 100.0 * 0x7FFF );
 	
-	tele_data[10] = (uint16_t)( (sensorData_ptr->adData_ptr->h) / 10000.0 * 0x7FFF );	// max AGL Alt.(m) = 10000 m
-	tele_data[11] = (uint16_t)( (sensorData_ptr->adData_ptr->ias) / 80.0 * 0x7FFF );	// max Indicated Airspeed(IAS) = 80 m/s	
+	tele_data[4] = (uint16_t)( (sensorData_ptr->adData_ptr->h) / 10000.0 * 0x7FFF );	// max AGL Alt.(m) = 10000 m
+	tele_data[5] = (uint16_t)( (sensorData_ptr->adData_ptr->ias) / 80.0 * 0x7FFF );	// max Indicated Airspeed(IAS) = 80 m/s	
 
-	tele_data[12] = (uint16_t)( navData_ptr->psi*R2D / 180.0 * 0x7FFF );	// Euler angles [psi,theta,phi] (deg)
-	tele_data[13]= (uint16_t)( navData_ptr->the*R2D / 90.0 * 0x7FFF );
-	tele_data[14]= (uint16_t)( navData_ptr->phi*R2D / 180.0 * 0x7FFF );
+	tele_data[6] = (uint16_t)( navData_ptr->psi*R2D / 180.0 * 0x7FFF );	// Euler angles [psi,theta,phi] (deg)
+	tele_data[7]= (uint16_t)( navData_ptr->the*R2D / 90.0 * 0x7FFF );
+	tele_data[8]= (uint16_t)( navData_ptr->phi*R2D / 180.0 * 0x7FFF );
 
-	tele_data[15]= (uint16_t)( controlData_ptr->l1 / 45 * 0x7FFF );	// control surface commands (normalized 0-1)
-	tele_data[16]= (uint16_t)( controlData_ptr->l2 / 45 * 0x7FFF );
-	tele_data[17]= (uint16_t)( controlData_ptr->l3 / 45 * 0x7FFF );
-	tele_data[18]= (uint16_t)( controlData_ptr->l4 / 45 * 0x7FFF );
-	tele_data[19]= (uint16_t)( controlData_ptr->l1 / 45 * 0x7FFF );
-	tele_data[20]= (uint16_t)( controlData_ptr->l2 / 45 * 0x7FFF );
-	tele_data[21]= (uint16_t)( controlData_ptr->l3 / 45 * 0x7FFF );
-	tele_data[22]= (uint16_t)( controlData_ptr->l4 / 45 * 0x7FFF );
-	tele_data[23]= (uint16_t)( controlData_ptr->dthr * 0x7FFF );
+	//tele_data[13]= (uint16_t)( controlData_ptr->l1 / L1_MAX * 0x7FFF );	// control surface commands (normalized 0-1)
+	tele_data[9]= (uint16_t)( controlData_ptr->l2 / L2_MAX * 0x7FFF );
+	tele_data[10]= (uint16_t)( controlData_ptr->l3 / L3_MAX * 0x7FFF );
+	tele_data[11]= (uint16_t)( controlData_ptr->l4 / L4_MAX * 0x7FFF );
+	//[17]= (uint16_t)( controlData_ptr->r1 / R1_MAX * 0x7FFF );
+	tele_data[12]= (uint16_t)( controlData_ptr->r2 / R2_MAX * 0x7FFF );
+	tele_data[13]= (uint16_t)( controlData_ptr->r3 / R3_MAX * 0x7FFF );
+	tele_data[14]= (uint16_t)( controlData_ptr->r4 / R4_MAX * 0x7FFF );
+	tele_data[15]= (uint16_t)( controlData_ptr->dthr * 0x7FFF );
 	
-	tele_data[24]= (uint16_t)( sensorData_ptr->accelData_ptr->cf / 8.0 * 0x7FFF );	// accelerometers = 8 g
-	tele_data[25]= (uint16_t)( sensorData_ptr->accelData_ptr->cr / 8.0 * 0x7FFF );
-	tele_data[26]= (uint16_t)( sensorData_ptr->accelData_ptr->lf / 8.0 * 0x7FFF );
-	tele_data[27]= (uint16_t)( sensorData_ptr->accelData_ptr->lr / 8.0 * 0x7FFF );
-	tele_data[28]= (uint16_t)( sensorData_ptr->accelData_ptr->rf / 8.0 * 0x7FFF );
-	tele_data[29]= (uint16_t)( sensorData_ptr->accelData_ptr->rr / 8.0 * 0x7FFF );
+	//tele_data[22]= (uint16_t)( sensorData_ptr->accelData_ptr->cf / 8.0 * 0x7FFF );	// accelerometers = 8 g
+	//tele_data[23]= (uint16_t)( sensorData_ptr->accelData_ptr->cr / 8.0 * 0x7FFF );
+	//tele_data[24]= (uint16_t)( sensorData_ptr->accelData_ptr->lf / 8.0 * 0x7FFF );
+	//tele_data[25]= (uint16_t)( sensorData_ptr->accelData_ptr->lr / 8.0 * 0x7FFF );
+	//tele_data[26]= (uint16_t)( sensorData_ptr->accelData_ptr->rf / 8.0 * 0x7FFF );
+	//tele_data[27]= (uint16_t)( sensorData_ptr->accelData_ptr->rr / 8.0 * 0x7FFF );
 	
-	tele_data[30]= (uint16_t)( 0 * 0x7FFF );
+	//tele_data[28] = cpuLoad;		/* cpuload */
 
-
-	tele_data[31] = cpuLoad;		/* cpuload */
-
-	tmp = (unsigned long)( sensorData_ptr->gpsData_ptr->lon *1e07 );
-	memcpy(&tele_data[32],&tmp,4);
-	tmp = (unsigned long)( sensorData_ptr->gpsData_ptr->lat *1e07 );
-	memcpy(&tele_data[33],&tmp,4);
+	//tmp = (unsigned long)( sensorData_ptr->gpsData_ptr->lon *1e07 );
+	//memcpy(&tele_data[29],&tmp,4);
+	//tmp = (unsigned long)( sensorData_ptr->gpsData_ptr->lat *1e07 );
+	//memcpy(&tele_data[31],&tmp,4);
 
 	if (missionData_ptr->recording == 1) flags = flags | 0x01<<0; // Logging
 	//if (missionData_ptr->mode == 1) flags = flags | 0x01<<0;  // Manual mode
@@ -112,21 +107,22 @@ void send_telemetry(struct sensordata *sensorData_ptr, struct nav *navData_ptr, 
 	if (sensorData_ptr->gpsData_ptr->err_type == data_valid || sensorData_ptr->gpsData_ptr->err_type == incompletePacket) flags = flags | 0x01<<7;
 	if (sensorData_ptr->gpsData_ptr->navValid == 0) flags = flags | 0x01<<8;
 	
-	tele_data[34] = flags;
-	tele_data[35] = (uint16_t)sensorData_ptr->gpsData_ptr->satVisible;
+	tele_data[16] = flags;
+	
+	tele_data[17] = (uint16_t)sensorData_ptr->gpsData_ptr->satVisible;
 	
 	
-	memcpy(&sendpacket[3],&tele_data,46);
+	memcpy(&sendpacket[3], &tele_data, SEND_PACKET_SIZE-5);
 	// compute 16-bit checksum of output data (excluding the header)
-	output_CKSUM = do_chksum (sendpacket, 2,TELE_PACKET_SIZE-2);
-	*(uint16_t *)&sendpacket[49] = output_CKSUM;
+	output_CKSUM = do_chksum (sendpacket, 2, SEND_PACKET_SIZE-2);
+	*(uint16_t *)&sendpacket[SEND_PACKET_SIZE-2] = output_CKSUM;
 
 	// send send_telemetry data packet to serial port
-	while(bytes != TELE_PACKET_SIZE) bytes += write(port, &sendpacket[bytes], TELE_PACKET_SIZE-bytes); bytes=0;
+	while(bytes != SEND_PACKET_SIZE) bytes += write(port, &sendpacket[bytes], TELE_PACKET_SIZE-bytes); bytes=0;
 	
 	// Send status message if present
 	if (statusMsg[0] != 0){
-		while(bytes != 103) bytes += write(port, &statusMsg[bytes], 103-bytes); bytes=0;
+		while(bytes != STATUS_PACKET_SIZE) bytes += write(port, &statusMsg[bytes], STATUS_PACKET_SIZE-bytes); bytes=0;
 		statusMsg[0] =0;
 	}
 }
