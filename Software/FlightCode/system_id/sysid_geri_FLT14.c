06c035040c69a2e1342415324c20b50ee83beb45
Chris Regan <rega0051@umn.edu>
2017-06-01 14:40:58 -0500

    .

--- Software/FlightCode/system_id/sysid_geri_FLT14.c
+++ Software/FlightCode/system_id/sysid_geri_FLT14.c
@@ -26,17 +26,17 @@ extern void get_system_id(double time, struct sensordata *sensorData_ptr, struct
 		case 0: 
 			// SysID#1
 			
-			if (time < 2.0){
+			if (time < 4.0){
 			}
-			else if((time >= 2.0) && (time < 14.0)){
-				controlData_ptr->surf3_excite = playback_OMS1_1(indx - 2.0*150, 5.1*D2R);
+			else if((time >= 4.0) && (time < 16.0)){
+				controlData_ptr->surf3_excite = playback_OMS1_1(indx - 4.0*150, 5.1*D2R);
 				if(overspeed == FALSE) {
 					controlData_ptr->l3    += controlData_ptr->surf3_excite;
 					controlData_ptr->r3    += controlData_ptr->surf3_excite;
 				}
 			}
 			
-			controlData_ptr->cmp_status = time / (14.0) * 100.0;
+			controlData_ptr->cmp_status = time / (16.0) * 100.0;
 			break;
 		case 1:
 			// SysID#2
